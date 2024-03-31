6b516dfd830eee079d32cc49d4db065ae81b3579
Suraj Kumar Manna <126694317+Suraj00345@users.noreply.github.com>
2024-03-31 17:29:48 +0530

    1st commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,54 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const addTodo = () => {
+    let found = false;
+    for (let i = 0; i < tasks.length; i++) {
+      let task = tasks[i];
+      if (task === inputValue) {
+        found = true;
+        break;
+      }
+    }
+
+    if (!found) {
+      setTasks([...tasks, inputValue]);
+      setInputValue("");
+    }
+  };
+
+  const removeTask = (taskTobeRemoved) => {
+    const remainingTasks = tasks.filter((task) => task !== taskTobeRemoved);
+    setTasks(remainingTasks);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <div>
+        <input
+          placeholder="Enter Todo"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button onClick={addTodo}>Add Todo</button>
+      </div>
+      <div>
+        {tasks.map((task) => {
+          return (
+            <ul>
+              <span style={{ marginRight: "100px" }}>{task}</span>
+              <button onClick={() => removeTask(task)}>Delete</button>
+            </ul>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
